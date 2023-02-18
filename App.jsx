ce4d0af5b1bb9a46846cac90c68bc680b0655758
VedantBhawsar <97240349+VedantBhawsar@users.noreply.github.com>
2023-02-18 06:58:17 +0530

    Icon Pack added

--- App.jsx
+++ App.jsx
@@ -1,24 +1,37 @@
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, StyleSheet, SafeAreaView, Button } from 'react-native'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
-
-
 const App = () => {
+  const [isTheme, setIstheme] = useState('light')
   return (
-    <ScrollView
-      style={{
-        flex: 1,
-        height: "100%",
-        backgroundColor: "black",
-      }}>
-      <View>
-        <Text style={{ color: "white" }}>App</Text>
-      </View>
-      {/* <Hello /> */}
-      <NavBar />
-    </ScrollView>
+    <SafeAreaView>
+      <NavBar isTheme={isTheme} setIsTheme={setIstheme} />
+    </SafeAreaView >
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: '25%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    paddingHorizontal: 15
+  },
+  logo: {
+    color: 'white',
+    fontWeight: '700',
+    fontFamily: 'Muli',
+    fontSize: 22
+  },
+  btn: {
+    flexDirection: 'row',
+    gap: 15
+  }
+})
 
 export default App
--- NavBar.jsx
+++ NavBar.jsx
@@ -1,12 +1,66 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React, { Component, useState } from 'react'
+import Icon from 'react-native-vector-icons/Foundation';
 
-const NavBar = () => {
+
+
+const NavBar = (props) => {
+    const { isTheme, setIsTheme } = props
+    console.log(props)
+
+    // const [isTheme, setIstheme] = useState('light')
+
     return (
-        <View>
-            <Text style={{ color: 'white' }}>hello</Text>
+        <View style={styles.darkContainer}>
+            {/* <Hello /> */}
+            <Text style={styles.logo}>Ig-Clone</Text>
+            <View style={styles.btns}>
+                <View style={styles.btn}>
+                    <Text style={{ color: 'white' }}>L</Text>
+                </View>
+                <View style={styles.btn}>
+                    <Icon name="heart" size={25} color="white"
+                    />
+                </View>
+                <View style={styles.btn}>
+                    <Text style={{ color: 'white' }}>L</Text>
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    darkContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        height: '25%',
+        fontFamily: 'Sacramento-Regular',
+        justifyContent: 'space-between',
+        backgroundColor: 'black',
+        alignItems: 'center',
+        paddingHorizontal: 16
+    },
+    logo: {
+        color: 'white',
+        fontWeight: '700',
+        fontFamily: 'Muli',
+        fontSize: 24,
+        alignItems: 'center',
+
+    },
+    btns: {
+        flexDirection: 'row',
+        gap: 10,
+        paddingVertical: 10
+    },
+    btn: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 35,
+        height: 35,
+        borderRadius: 4
+    }
+})
 
 export default NavBar
